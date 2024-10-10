eb7a387895cd28ccbd74d87fc76aee685ae737af
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-10 14:16:59 +0000

    solve the last mandatory task errors

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -8,4 +8,5 @@ from typing import List, Tuple, Sequence, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    'return an elements of the list'
     return [(i, len(i)) for i in lst]
