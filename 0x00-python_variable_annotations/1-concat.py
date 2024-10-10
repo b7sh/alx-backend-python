d760fa44ec394759dfe87905e93fe4c304315e03
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-10 11:53:15 +0000

    solve the second task '1. Basic annotations - concat'

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+a type-annotated function concat
+that takes tow strings as arguments
+and returns a concatenated string
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    'return a string'
+    return str1 + str2
