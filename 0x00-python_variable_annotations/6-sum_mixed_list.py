3113fd9b340213f5841536d48f84636eb297128a
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-10 13:17:00 +0000

    solve the seventh task errors

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,9 +4,9 @@ a type-annotated function sum_mixed_list
 which takes a list mxd_lst of integers and floats
 and returns their sum as a float.
 '''
-from typing import List, Any
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Any]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     'return sum of floats'
     return sum(mxd_lst)
