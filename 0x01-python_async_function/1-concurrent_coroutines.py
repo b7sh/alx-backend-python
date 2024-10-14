9b30d071ba3c1e2a206b75eaaa2f225c067b7499
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-14 10:27:54 +0000

    solve the secod task errors

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 '''
+Import wait_random from the previous python file
+that you’ve written and write an async routine
+called wait_n that takes in 2 int arguments
+(in this order): n and max_delay.
+You will spawn wait_random n times
+with the specified max_delay.
 '''
 import asyncio
 from typing import List
@@ -7,6 +13,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    'eturn the list of all the delays (float values).'
     delays = []
 
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
